51bd6598730f18ce4dc2c12b789579c10c162af0
ruzsaz <Zoltan.Ruzsa@genomate.helath>
2023-08-08 23:48:42 +0200

    Initial commit

---
+++ src/main/java/hu/agnos/cube/driver/NativeStatement.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.agnos.cube.driver;
+
+import hu.agnos.molap.Cube;
+import hu.agnos.cube.driver.zolikaokos.DataRetriever;
+import hu.agnos.cube.driver.zolikaokos.Problem;
+import hu.agnos.cube.driver.zolikaokos.ResultElement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author parisek
+ */
+public class NativeStatement {
+
+    private Cube cube;
+
+    public NativeStatement(Cube cube) {
+        this.cube = cube;
+    }
+
+    public ResultSet[] executeQueries(String baseVector, String[] drillVectors) {
+        int drillVectorsSize = drillVectors.length;
+        
+        
+        
+        ResultSet[] resultSet = new ResultSet[drillVectorsSize];        
+        DataRetriever retriever = new DataRetriever(cube);
+
+        for (int i = 0; i < drillVectors.length; i++) {
+            String[] newBaseVectorArray = null;
+            String drillVector = drillVectors[i];
+            
+            resultSet[i] = new ResultSet(drillVector);
+            
+            if (drillVector != null) {
+                String[] baseVectorArray = baseVector.split(":", -1);
+                String[] drillVectorArray = drillVector.split(":", -1);
+                NativeQueryGenerator queryGenerator = new NativeQueryGenerator(cube.getDimensions(), cube.getHierarchyIndex());
+                newBaseVectorArray = queryGenerator.getBaseVectorsFromDrillVector(baseVectorArray, drillVectorArray);
+            }
+
+            if (newBaseVectorArray != null) {
+                int rowCnt = newBaseVectorArray.length;
+                for (int j = 0; j < rowCnt; j++) {
+                    retriever.addProblem(new Problem(i, newBaseVectorArray[j]));
+                }
+            }
+        }
+        
+        List<Future<ResultElement>> futures = retriever.computeAll();
+        
+        
+        List<List<ResultElement>>  eredmeny = new ArrayList<>();
+        for(int i= 0; i < drillVectorsSize; i++){
+            List<ResultElement> temp = new ArrayList<>();
+            eredmeny.add(temp);
+        }
+        
+         
+        
+        
+        for(int i = 0; i < futures.size(); i++){
+             try {
+                 
+                    ResultElement r = futures.get(i).get();
+//                    for(String s : r.getHeader()){
+//                        System.out.print(s + ", ");
+//                    }
+//                    System.out.println("");
+                    int drillVectorId = r.getDrillVectorId();
+                    List<ResultElement> temp = eredmeny.get(drillVectorId);
+                    temp.add(r);                    
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(NativeStatement.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ExecutionException ex) {
+                    Logger.getLogger(NativeStatement.class.getName()).log(Level.SEVERE, null, ex);
+                }
+        }
+        
+      
+        for(int i = 0; i < drillVectorsSize; i++){
+            resultSet[i].setResponse(eredmeny.get(i));         
+        }
+        
+        return resultSet;
+    }
+
+    public ResultSet executeQuery(String baseVector, String drillVector) {
+        ResultSet resultSet = null;
+        String[] newBaseVectorArray = null;
+
+        if (drillVector != null) {
+            String[] baseVectorArray = baseVector.split(":", -1);
+            String[] drillVectorArray = drillVector.split(":", -1);
+            NativeQueryGenerator queryGenerator = new NativeQueryGenerator(cube.getDimensions(), cube.getHierarchyIndex());
+
+            newBaseVectorArray = queryGenerator.getBaseVectorsFromDrillVector(baseVectorArray, drillVectorArray);
+
+        }
+
+        if (newBaseVectorArray != null) {
+            resultSet = new ResultSet(drillVector);
+            ResultElement[] response = null;
+            int rowCnt = newBaseVectorArray.length;
+
+            DataRetriever retriever = new DataRetriever(cube);
+            for (int i = 0; i < rowCnt; i++) {
+                retriever.addProblem(new Problem(0, newBaseVectorArray[i]));
+            }
+
+            List<Future<ResultElement>> futures = retriever.computeAll();
+
+            response = new ResultElement[rowCnt];
+
+            for (int i = 0; i < rowCnt; i++) {
+                try {
+                    response[i] = futures.get(i).get();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(NativeStatement.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ExecutionException ex) {
+                    Logger.getLogger(NativeStatement.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+//            resultSet.setResponse(response);
+
+        }
+        return resultSet;
+    }
+
+}
---
+++ src/main/java/hu/agnos/cube/driver/ResultSet.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.agnos.cube.driver;
+
+import hu.agnos.cube.driver.zolikaokos.ResultElement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author parisek
+ */
+public class ResultSet implements java.io.Serializable {
+
+    private static final long serialVersionUID = -8940196742313994740L;
+    private final String name;
+    private List<ResultElement> response;
+
+    public ResultSet(String name) {
+        this.name = name;
+        this.response = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<ResultElement> getResponse() {
+        return response;
+    }
+
+    public void addResponse(ResultElement result) {
+        this.response.add(result);
+    }
+
+    public void setResponse(List<ResultElement> response) {
+        this.response = response;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("ResultSet{" + "name=" + name + ", response:");
+        for (ResultElement e : this.response) {
+            sb.append("\n\t").append(e.toString());
+        }
+        sb.append("\n}");
+        return sb.toString();
+    }
+
+    public ResultSet deepCopy() {
+        ResultSet result = new ResultSet(new String(name));
+        for (ResultElement r : this.response) {
+            result.addResponse(r);
+        }
+        return result;
+    }
+
+}
